7c1e549d7d2c56c30c2effd208674cda0ee73c28
0xErr420 <0xErr420>
2022-10-11 23:09:29 +0200

    using Reader module in CUT.c

--- src/CUT.c
+++ src/CUT.c
@@ -1,96 +1,39 @@
+#include "reader.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <string.h>
+#include <semaphore.h>
 
 // Using enum for better code readability in the future
-typedef enum Thread
+enum Thread
 {
-    Reader,   // 0
-    Analyzer, // 1
-    Printer,  // 2
-    Watchdog, // 3
-    Logger    // 4
-} Thread;
+    Reader = 1, // 1
+    Analyzer,   // 2
+    Printer,    // 3
+    Watchdog,   // 4
+    Logger      // 5
+};
 
-#define THREAD_NUM 5 // Reader, Analyzer, Printer, Watchdog, Logger
-
-char stat_cpu[1024];
-
-/// I SHOULD READ USAGE FOR EACH CPU!
-/// I CAN USE GLOBAL variable/struct TO SHARE DATA BETWEEN THREADS
-
-void *open_proc_stat()
-{
-    printf("Opening /proc/stat file...\n");
-
-    FILE *file = fopen("/proc/stat", "r");
-    if (!file)
-    {
-        perror("Could not open stat file");
-        exit(EXIT_FAILURE);
-    }
-    printf("Opened /proc/stat file\n");
-    return file;
-}
-
-void *thread_Reader()
-{
-    /// Every time i want to read new stats, I NEED TO REOPEN /proc/stat file!
-    /// Maybe I should implement opening file and reading from file as ONE function? (and write unit test for it)
-
-    /// While loop
-    /// 1. Open file
-    /// 2. Read from file
-    /// 3. Send Raw/Extracted data to another thread
-    /// 4. repeat
-
-    /// TODO: BUFFER "cpu" objects extracted from /proc/stat
-    /// TODO: Implement Producer-Consumer (DO NOT USE spinlocks)
-
-    printf("Reader thread started\n");
-    /// TODO: CHANGE IT TO WHILE LOOP
-    for (int i = 0; i < 10; i++)
-    {
-        printf("Iteration No-%d\n", i);
-
-        // Open file
-        FILE *pFile = open_proc_stat();
-
-        // Read every line from file which starts with "cpu"
-        while (1)
-        {
-            char str[224];
-            // Read the line and compare if it starts with "cpu"
-            if ((fgets(str, 224, pFile) == NULL) | (strncmp(str, "cpu", (size_t)3) != 0))
-            {
-                // Does not start with "cpu" -> break loop (because it is last "cpu" line)
-                // printf("fgets() == NULL or strncmp() != 0");
-                break;
-            }
-            // Starts with "cpu" -> continue
-            printf("%s", str);
-
-            /// TODO: Send string to Analyzer
-        }
-        printf("\n");
-
-        // Close file and sleep for some time
-        fclose(pFile);
-        sleep(1);
-    }
-    printf("Reader thread closes\n");
-    return NULL;
-}
+/// Array of threads identifiers should be 5 elements long
+#define THREAD_NUM (5) // Reader, Analyzer, Printer, Watchdog, Logger
 
 int main()
 {
+    // Array of thread identifiers
     pthread_t thread[THREAD_NUM];
 
-    pthread_create(&thread[Reader], NULL, &thread_Reader, NULL);
+    // Start threads
+    if (pthread_create(&thread[Reader], NULL, &thread_Reader, NULL) != 0)
+    {
+        perror("Failed to create thread");
+    }
 
-    pthread_join(thread[Reader], NULL);
+    // Close threads
+    if (pthread_join(thread[Reader], NULL) != 0)
+    {
+        perror("Failed to join thread");
+    }
 
     // printf("Print stat_cpu:\n%s\n", stat_cpu);
 

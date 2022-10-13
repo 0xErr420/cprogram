f82f2decd47f39faaff40114daacf473a88e218d
0xErr420 <0xErr420>
2022-10-13 13:29:04 +0200

    setup for Producer-Consumer

--- src/CUT.c
+++ src/CUT.c
@@ -1,7 +1,9 @@
 #include "reader.h"
+#include "utils.h"
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h>
 #include <semaphore.h>
 
@@ -16,24 +18,87 @@ enum Thread
 };
 
 /// Array of threads identifiers should be 5 elements long
-#define THREAD_NUM (5) // Reader, Analyzer, Printer, Watchdog, Logger
+#define THREAD_NUM 5 // Reader, Analyzer, Printer, Watchdog, Logger
+
+#define RA_BUFF_SIZE 10
+sem_t sem_RA_Empty;
+sem_t sem_RA_Filled;
+pthread_mutex_t mutex_RA_buffer;
+circular_buffer RA_buffer;
+// int *RA_buffer[RA_BUFF_SIZE]; // Array of char pointers
+// int RA_count = 0;             // Pointing to the next after last element
+
+void *thread_Producer()
+{
+    while (1)
+    {
+        // Produce
+        sem_wait(&sem_RA_Empty); // Wait for empty
+        pthread_mutex_lock(&mutex_RA_buffer);
+
+        pthread_mutex_unlock(&mutex_RA_buffer);
+        sem_post(&sem_RA_Filled); // Tell other thread there is filled available
+    }
+}
+
+/// Calculate CPU usage (in percentages) for each CPU core from /proc/stat.
+void *thread_Analyzer()
+{
+    /// Consumer for RA_buffer:
+    /// 1. Wait for filled element in buffer (increments "filled" semaphore)
+    /// 2. Lock mutex
+    /// 3. Do some calculation
+    /// 4. Unlock mutex
+    /// 5. Decrement "empty" semaphore
+
+    while (1)
+    {
+        // Consume
+        sem_wait(&sem_RA_Filled); // Wait for filled
+        pthread_mutex_lock(&mutex_RA_buffer);
+
+        pthread_mutex_unlock(&mutex_RA_buffer);
+        sem_post(&sem_RA_Empty); // Tell other thread there is empty available
+    }
+}
 
 int main()
 {
+    srand(time(NULL));
     // Array of thread identifiers
     pthread_t thread[THREAD_NUM];
 
+    /// -------------------------------
+    // Initialization for Read-Analyzer
+    pthread_mutex_init(&mutex_RA_buffer, NULL);
+    sem_init(&sem_RA_Empty, 0, RA_BUFF_SIZE);
+    sem_init(&sem_RA_Filled, 0, 0);
+    cb_init(&RA_buffer, RA_BUFF_SIZE, sizeof(char));
+
     // Start threads
-    if (pthread_create(&thread[Reader], NULL, &thread_Reader, NULL) != 0)
+    // if (pthread_create(&thread[Reader], NULL, &thread_Reader, NULL) != 0)
+    // {
+    //     perror("Failed to create Reader thread");
+    // }
+
+    // Remove this thread in the future (it is just a test for producer role)
+    pthread_create(&thread[1], NULL, &thread_Producer, NULL);
+
+    if (pthread_create(&thread[Analyzer], NULL, &thread_Analyzer, NULL) != 0)
     {
-        perror("Failed to create thread");
+        perror("Failed to create Analyzer thread");
     }
+    /// -------------------------------
 
     // Close threads
     if (pthread_join(thread[Reader], NULL) != 0)
     {
         perror("Failed to join thread");
     }
+    cb_free(&RA_buffer);
+    sem_destroy(&sem_RA_Empty);
+    sem_destroy(&sem_RA_Filled);
+    pthread_mutex_destroy(&mutex_RA_buffer);
 
     // printf("Print stat_cpu:\n%s\n", stat_cpu);
 

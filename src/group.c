2869dd693abca5570cc823429c476be4bc2d5f7a
0xErr420 <0xErr420>
2022-10-19 01:12:48 +0200

    new module: group data structure

---
+++ src/group.c
@@ -0,0 +1,55 @@
+#include "group.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+int group_init(group *grp, size_t capacity, size_t sz)
+{
+    grp->pgrp = malloc(capacity * sz);
+    if (grp->pgrp == NULL)
+    {
+        return -1;
+    }
+    grp->capacity = capacity;
+    grp->size = sz;
+    grp->count = 0;
+    grp->uncount = 0;
+    return 0;
+}
+
+void group_free(group *grp)
+{
+    free(grp->pgrp);
+}
+
+int group_push(group *grp, const void *item)
+{
+    if (grp->count >= grp->capacity)
+    {
+        return -1;
+    }
+    memcpy(grp->pgrp + (grp->size * grp->count), item, grp->size);
+    grp->count++;
+    return 0;
+}
+
+int group_pop(group *grp, void *item)
+{
+    if (grp->uncount >= grp->count)
+    {
+        return -1;
+    }
+    memcpy(item, grp->pgrp + (grp->size * grp->uncount), grp->size);
+    grp->uncount++;
+    return 0;
+}
+
+int group_get(group *grp, void *item, int n)
+{
+    if (n >= grp->count || n < 0)
+    {
+        return -1;
+    }
+    memcpy(item, grp->pgrp + (grp->size * n), grp->size);
+    return 0;
+}
---
+++ src/group.h
@@ -0,0 +1,49 @@
+#ifndef GROUP_H
+#define GROUP_H
+#include <stddef.h>
+
+/// Group structure
+typedef struct group
+{
+    void *pgrp;   // pointer to allocated memory (capacity * size)
+    int capacity; // amount of elements
+    int size;     // size of each element
+    int count;    // count how many elements already added
+    int uncount;  // used for pop operation
+} group;
+
+/// Initialize group
+/// Dynamically allocates memory, it is necessary to call the function `group_free(...)`.
+///
+/// CAPACITY is how many elements in group, SZ is size of individual element
+///
+/// returns 0 if successful, -1 if failed
+int group_init(group *grp, size_t capacity, size_t sz);
+
+/// Free dynamically allocated memory for group
+void group_free(group *grp);
+
+/// Push element to group
+///
+/// ITEM is element to add
+///
+/// returns 0 if successful, -1 if failed
+int group_push(group *grp, const void *item);
+
+/// Pop element from group
+/// (pops from group, use group_get to only get element)
+///
+/// ITEM used to return value
+///
+/// returns 0 if successful, -1 if failed
+int group_pop(group *grp, void *item);
+
+/// Get N element from group
+/// (dont pop, just to get)
+///
+/// N specifies element, ITEM used to return value
+///
+/// returns 0 if successful, -1 if failed
+int group_get(group *grp, void *item, int n);
+
+#endif

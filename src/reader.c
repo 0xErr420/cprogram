333ad4c83691c4265108edb03b807be582b3e2e1
0xErr420 <0xErr420>
2022-10-20 19:00:12 +0200

    Reader module: full refactor, redesign

--- src/reader.c
+++ src/reader.c
@@ -1,92 +1,133 @@
 #include "reader.h"
 #include "utils.h"
+#include "circular_buffer.h"
+#include "group.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-/// Open /proc/stat file
-///
-/// returns FILE pointer if successful, NULL if failed
-static void *open_proc_stat(void);
-
-// /// Read /proc/stat file
-// void *read_proc_stat(FILE *file);
-
 void *thread_Reader(void *arg)
 {
-    /// 1. Read 'cpu' lines
-    /// 2. Send each line to Analyzer
-    /// 3. repeat
-
     /// TODO: remove any printf(), check for return values, handle errors
     ArgsThread_type *args = (ArgsThread_type *)(arg);
-    while (1)
+    while (1) // Thread loop
     {
-        /// Produce:
-        FILE *proc = open_proc_stat();
+        // Get available number of cpus
+        long num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+        if (num_cpus == -1)
+        {
+            /// TODO: Signal to watchdog about this problem (-1 for example, would mean thread failed)
+            perror("Failed to call sysconf(_SC_NPROCESSORS_ONLN) to get available CPUs");
+            break; // Exit thread loop
+        }
+
+        // Wait for empty and then post it back
+        // (In other words, produce only after empty producer slot appeared)
+        sem_wait(&args->arg2->sem_empty); // Wait for empty
+        sem_post(&args->arg2->sem_empty); // Post empty
+
+        // Initialize 'g_cpus' group for cpu_fields
+        group g_cpus;
+        if (group_init(&g_cpus, num_cpus + 1, sizeof(cpu_fields)) == -1)
+        {
+            /// TODO: Signal to watchdog about this problem (-1 for example, would mean thread failed)
+            perror("Failed to initialize group g_cpus");
+            break; // Exit thread loop
+        }
+
+        /// ==== Produce into buffer ====
+        // Open file
+        FILE *stat_file = fopen("/proc/stat", "r");
+        if (stat_file == NULL)
+        {
+            /// TODO: Signal to watchdog about this problem (-1 for example, would mean thread failed)
+            perror("Failed to open /proc/stat file");
+            break; // Exit thread loop
+        }
+
+        // Read file and store extracted cpus into group
         while (1)
         {
-            char str[CPU_READ_SIZE];
-            // Read the line and compare if it starts with "cpu"
-            if (fgets(str, CPU_READ_SIZE, proc) == NULL)
+            char str[220];  // Buffer used for reading line from file
+            cpu_fields cpu; // Strcucture used for extracted cpu fields
+
+            // Read the line
+            if (fgets(str, sizeof(str), stat_file) == NULL)
             {
-                // fgets(): If the End-of-File is encountered and no characters have been read, a null pointer is returned
+                // man fgets(): If the End-of-File is encountered and no characters have been read, a null pointer is returned
                 perror("Failed to read /proc/stat file");
-                break;
+                break; // Exit read loop
             }
+            // Compare if line starts with "cpu"
             if (strncmp(str, "cpu", (size_t)3) != 0)
             {
                 // Does not start with "cpu" -> break loop (previous line was last "cpu" line)
-                break;
+                break; // Exit read loop
             }
-            // Starts with "cpu" -> continue
+            // Line starts with "cpu" -> continue
 
-            sem_wait(&args->arg2->sem_empty); // Wait for empty
-            pthread_mutex_lock(&args->arg2->mutex_buffer);
+            // Extract cpu fields into struct
+            if (strncmp(str, "cpu ", (size_t)4) == 0) // if "cpu_" has space instead of digit
+            {
+                cpu.cpu_id = -1; // it is total cpu
+                // extract fields of cpu
+                int r = sscanf(str,
+                               "cpu  %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+                               &cpu.user, &cpu.nice, &cpu.system, &cpu.idle, &cpu.iowait, &cpu.irq, &cpu.softirq, &cpu.steal, &cpu.guest, &cpu.guest_nice);
+                if (r < 4)
+                {
+                    fprintf(stderr, "Failed to extract cpu fields, sscanf() returned %d, minimum 4 expected", r);
+                    break; // Exit read loop
+                }
+            }
+            else // if "cpu_" has digit
+            {
+                // extract id of cpu
+                int r = sscanf(str, "cpu%d ", &cpu.cpu_id);
+                if (r <= 0)
+                {
+                    fprintf(stderr, "Failed to read cpu id field, sscanf() returned %d, 1 expected", r);
+                    break; // Exit read loop
+                }
+                // extract fields of cpu
+                r = sscanf(str,
+                           "cpu%*d  %llu %llu %llu %llu %llu %llu %llu %llu %llu %llu",
+                           &cpu.user, &cpu.nice, &cpu.system, &cpu.idle, &cpu.iowait, &cpu.irq, &cpu.softirq, &cpu.steal, &cpu.guest, &cpu.guest_nice);
+                if (r < 4)
+                {
+                    fprintf(stderr, "Failed to extract cpu fields, sscanf() returned %d, minimum 4 expected", r);
+                    break; // Exit read loop
+                }
+            }
 
-            if (cb_push_back(&args->arg2->buffer, str) != 0)
-                perror("Failed to add element to circular buffer");
-            printf("Produced: %s", str);
+            // Add cpu to group
+            if (group_push(&g_cpus, &cpu) == -1)
+            {
+                perror("Failed to push element to g_cpus group");
+                break; // Exit read loop
+            }
 
-            pthread_mutex_unlock(&args->arg2->mutex_buffer);
-            sem_post(&args->arg2->sem_filled); // Tell other thread there is filled available
-        }
-        fclose(proc);
-        /// TODO: remove sleep
-        sleep(1);
+        } // End of read file loop
+        fclose(stat_file);
+
+        // Send to another thread
+        sem_wait(&args->arg2->sem_empty); // Wait for empty
+        pthread_mutex_lock(&args->arg2->mutex_buffer);
+
+        if (cb_push_back(&args->arg2->buffer, &g_cpus) != 0)
+            perror("Failed to add element to circular buffer");
+
+        pthread_mutex_unlock(&args->arg2->mutex_buffer);
+        sem_post(&args->arg2->sem_filled); // Tell other thread there is filled available
 
         // Test if there was a signal to exit
         pthread_testcancel();
-    }
+
+    } // End of thread loop
+
+    /// TODO: In case of thread exit, free all produced elements stored in buffer
+
     return NULL;
 }
-
-static void *open_proc_stat(void)
-{
-    /// TODO: refactor, remove printf(), handle errors and return value
-    printf("Opening /proc/stat file...\n");
-
-    FILE *file = fopen("/proc/stat", "r");
-    if (!file)
-    {
-        perror("Could not open stat file");
-        return NULL;
-    }
-
-    printf("Opened /proc/stat file\n");
-    return file;
-}
-
-// void *read_proc_stat(FILE *file)
-// {
-//     /// TODO: file read loop
-//     /// 1. Open file
-//     /// 2. Read from file loop:
-//     ///     2.1 Read line
-//     ///     2.2 If line starts with "cpu" -> continue, else -> break loop (go to point 4)
-//     ///     2.3 Send line to buffer for another thread
-//     /// 4. Close file
-//     /// 5. repeat
-// }
--- src/reader.h
+++ src/reader.h
@@ -1,13 +1,9 @@
 #ifndef READER_H
 #define READER_H
 
-/// String size of 'cpu' line
+/// Reader thread: reads 'cpu' data and sends them to specified buffer
 ///
-/// Should be enough, assuming that in the file /proc/stat the line 'cpu' has a maximum of 10 fields
-/// calculated: <MAX_unsigned_long_long> * <max number of fileds in 'cpu' line> + <sizeof("cpu ")>
-#define CPU_READ_SIZE 224UL
-
-/// Reader thread reads 'cpu' data and sends them to specified buffer
+/// ARG fields required: Producer
 void *thread_Reader(void *arg);
 
 #endif

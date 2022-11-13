956288e69ca3d7ae545e7d1e7f3ab2b8465169bd
0xErr420 <0xErr420>
2022-11-13 19:39:40 +0100

    analyzer: refactor and using consumer_producer

--- src/analyzer.c
+++ src/analyzer.c
@@ -1,5 +1,6 @@
 #include "analyzer.h"
 #include "utils.h"
+#include "consume_produce.h"
 #include "group.h"
 #include "circular_buffer.h"
 
@@ -8,107 +9,107 @@
 #include <unistd.h>
 #include <string.h>
 
+/// Calculate usage percentage for cpu
+static void calc_percentage(cpu_fields *cpu, cpu_usage *perc);
+
 void *thread_Analyzer(void *arg)
 {
-    /// TODO: remove any printf(), check for return values, handle errors
-    ArgsThread_type *args = (ArgsThread_type *)(arg);
+    args_thread *args = (args_thread *)(arg);
     while (1) // Thread loop
     {
-        // Wait for empty and then post it back
-        // (In other words, consume and produce only after empty producer slot appeared)
-        sem_wait(&args->arg2->sem_empty); // Wait for empty
-        sem_post(&args->arg2->sem_empty); // Post empty
+        // Wait for empty and then post it back (consume and produce only after empty producer slot appeared)
+        sem_wait(&args->arg2->sem_empty);
+        sem_post(&args->arg2->sem_empty);
 
-        /// ==== Consume from buffer ====
-
-        // Group for cpu_fields to consume group from buffer
+        // Group used to consume from buffer
         group g_cpus;
-
-        // Receive group from buffer
-        sem_wait(&args->arg1->sem_filled); // Wait for filled
-        pthread_mutex_lock(&args->arg1->mutex_buffer);
-
-        if (cb_pop_front(&args->arg1->buffer, &g_cpus) != 0)
-            perror("Failed to get element from circular buffer");
-
-        pthread_mutex_unlock(&args->arg1->mutex_buffer);
-        sem_post(&args->arg1->sem_empty); // Tell other thread there is empty available
-
-        // Initialize 'cpu_percentages' group for cpu_usage
+        // Group used to produce to buffer
         group g_percentages;
-        if (group_init(&g_percentages, g_cpus.count, sizeof(cpu_usage)) == -1)
+
+        // Consume group with cpu fields
+        if (cp_consume(args->arg1, &g_cpus) != 0)
         {
-            /// TODO: Signal to watchdog about this problem (-1 for example, would mean thread failed)
-            perror("Failed to initialize group g_percentages");
-            break; // Exit thread loop
+            perror("Failed to consume");
+            return NULL;
         }
 
-        /// ==== Produce into buffer ====
-        // Loop for each cpu in 'g_cpus' group, calculate percentage and add to 'g_percentages' group
+        // Initialize group for cpu percentages
+        if (group_init(&g_percentages, g_cpus.count, sizeof(cpu_usage)) != 0)
+        {
+            /// TODO: Signal to watchdog
+            perror("Failed to initialize group");
+            group_free(&g_cpus);
+            return NULL;
+        }
+
+        // Loop for each cpu in group, calculate percentage and add to percentages group
         for (int i = 0; i < g_cpus.count; i++)
         {
             cpu_fields cpu;           // Strcucture used to pop cpu from group
             cpu_usage cpu_percentage; // Structure used to store calculated percentages
 
             // Pop cpu from group
-            if (group_pop(&g_cpus, &cpu) == -1)
+            if (group_pop(&g_cpus, &cpu) != 0)
             {
-                perror("Failed to pop element from g_cpus group");
-                break; // Exit for loop
+                perror("Failed to pop element from group");
+                group_free(&g_percentages);
+                group_free(&g_cpus);
+                return NULL;
             }
-
-            // Simplified version of percentage calculation
-            unsigned long long idle = cpu.idle + cpu.iowait;
-            unsigned long long non_idle = cpu.user + cpu.nice + cpu.system + cpu.irq + cpu.softirq + cpu.steal;
-
-            double total = idle + non_idle;
-            double usage_percent = (non_idle * 100) / total;
-
-            cpu_percentage.cpu_id = cpu.cpu_id;
-            cpu_percentage.percentage = usage_percent;
+            // Calculate percentage
+            calc_percentage(&cpu, &cpu_percentage);
 
             // Add percentage to group
             if (group_push(&g_percentages, &cpu_percentage) == -1)
             {
-                perror("Failed to push element to g_percentages group");
-                break; // Exit for loop
+                perror("Failed to push element to group");
+                group_free(&g_percentages);
+                group_free(&g_cpus);
+                return NULL;
             }
-        } // End of for loop
-
-        // Free resources of consumed 'g_cpus' group
+        }
+        // Free resources of consumed group
         group_free(&g_cpus);
 
         // Send to another thread
-        sem_wait(&args->arg2->sem_empty); // Wait for empty
-        pthread_mutex_lock(&args->arg2->mutex_buffer);
-
-        if (cb_push_back(&args->arg2->buffer, &g_percentages) != 0)
-            perror("Failed to add element to circular buffer");
-
-        pthread_mutex_unlock(&args->arg2->mutex_buffer);
-        sem_post(&args->arg2->sem_filled); // Tell other thread there is filled available
+        if (cp_produce(args->arg2, &g_percentages) != 0)
+        {
+            perror("Failed to produce");
+            group_free(&g_percentages);
+            return NULL;
+        }
 
         // Test if there was a signal to exit
         pthread_testcancel();
 
-        // /// Calculate percentage for current cpu
-        // unsigned long long PrevIdle = prev_idle + prev_iowait;
-        // unsigned long long PrevNonIdle = prev_user + prev_nice + prev_system + prev_irq + prev_softirq + prev_steal;
-
-        // unsigned long long Idle = idle + iowait;
-        // unsigned long long NonIdle = user + nice + system + irq + softirq + steal;
-
-        // unsigned long long PrevTotal = PrevIdle + PrevNonIdle;
-        // unsigned long long Total = Idle + NonIdle;
-
-        // double TotalD = Total = PrevTotal;
-        // double IdleD = Idle - PrevIdle;
-
-        // double CpuPercentage = (TotalD - IdleD) / TotalD;
-
     } // End of thread loop
-
-    /// TODO: In case of thread exit, free all produced elements stored in buffer
-
     return NULL;
+}
+
+static void calc_percentage(cpu_fields *cpu, cpu_usage *perc)
+{
+    // Simplified version of percentage calculation
+    unsigned long long idle = cpu->idle + cpu->iowait;
+    unsigned long long non_idle = cpu->user + cpu->nice + cpu->system + cpu->irq + cpu->softirq + cpu->steal;
+
+    double total = idle + non_idle;
+    double usage_percent = (non_idle * 100) / total;
+
+    perc->cpu_id = cpu->cpu_id;
+    perc->percentage = usage_percent;
+
+    // /// Calculate percentage for cpu
+    // unsigned long long PrevIdle = prev_idle + prev_iowait;
+    // unsigned long long PrevNonIdle = prev_user + prev_nice + prev_system + prev_irq + prev_softirq + prev_steal;
+
+    // unsigned long long Idle = idle + iowait;
+    // unsigned long long NonIdle = user + nice + system + irq + softirq + steal;
+
+    // unsigned long long PrevTotal = PrevIdle + PrevNonIdle;
+    // unsigned long long Total = Idle + NonIdle;
+
+    // double TotalD = Total = PrevTotal;
+    // double IdleD = Idle - PrevIdle;
+
+    // double CpuPercentage = (TotalD - IdleD) / TotalD;
 }
